c7201a2a243ea05959bfbee154e6dd0d9d86105e
DavidTobonIBIO <d.tobonm2@uniandes.edu.co>
2024-11-22 12:18:55 -0500

    prerelease

---
+++ src/app/season/season.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SeasonComponent } from './season.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SeasonComponent]
+})
+export class SeasonModule { }
---
+++ src/app/season/season.ts
@@ -0,0 +1,12 @@
+export class Season {
+    name: string;
+    chapters: number;
+
+    constructor(
+        name: string,
+        chapters: number,
+    ) {
+        this.name = name;
+        this.chapters = chapters;
+    }
+}
